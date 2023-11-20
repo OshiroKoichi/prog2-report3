13ab0c8aaa111fb7f1a0d2255319d1a945ac84d8
tameikip <e235707@ie.u-ryukyu.ac.jp>
2023-11-20 21:21:48 +0900

    report4

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -5,15 +5,28 @@ public class Main {
         Hero hero = new Hero("勇者", 10, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
 
-        System.out.printf("%s vs. %s\n", hero.name, enemy.name);
+        System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
         int turn = 0;
-        while( hero.dead == false && enemy.dead == false ){
+        while( !hero.isDead() == false && enemy.isDead() == false ){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
             enemy.attack(hero);
         }
         System.out.println("戦闘終了");
+    }
+
+    public static void attackTest(Hero hero, Enemy enemy) {
+        int defaultHeroHp = hero.getHitPoint();
+        hero.attack(enemy);
+        enemy.attack(hero);
+
+        // アサーションを実行
+        if (defaultHeroHp == hero.getHitPoint()) {
+            System.out.println("攻撃テストが成功しました！");
+        } else {
+            System.out.println("攻撃テストが失敗しました！");
+        }
     }
 }
